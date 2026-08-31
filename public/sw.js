ab9619a8fb070e12bbd17a1b756bb8f855a67877
CAP-Mantenimiento <hector.aguilar@cap.edu.mx>
2026-08-31 12:45:23 -0600

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'mantenimiento-cap-v1';
+
+const APP_SHELL = [
+  '/',
+  '/manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
